0ce6eef5b02328aa3416bfc1ae71ad3253a75db3
Brad Traversy <traversymedia@gmail.com>
2018-02-26 11:05:05 -0500

    Add profile experiences route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -98,4 +98,32 @@ router.get('/user/:user_id', (req, res) => {
     );
 });
 
+// POST /profile/experience [Add Experience (Private)]
+router.post(
+  '/experience',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    Profile.findOne({
+      user: req.user.id
+    }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.title,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      // Add to exp array
+      profile.experience.unshift(newExp);
+
+      profile.save().then(profile => {
+        res.json({ success: true, msg: 'Experience added', profile });
+      });
+    });
+  }
+);
+
 module.exports = router;
